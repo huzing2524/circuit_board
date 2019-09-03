1e6bdfee4545e369ac9975053325b7116798ac34
huzing2524 <826956010@qq.com>
2019-09-03 10:40:35 +0800

    update 9.3 10:40

--- create.sql
+++ create.sql
@@ -2,6 +2,7 @@ create table templates
 (
   id           serial primary key,
   shape        varchar(10),
-  top_left     float[], -- 左上
-  bottom_right float[]  -- 右下
+  top_left     float[],   -- 左上
+  bottom_right float[],   -- 右下
+  direction    varchar(1) -- 水平: 0, 垂直: 1
 );
